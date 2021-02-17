9e01f5b56e4dea5b1295cfb228ec6ef93df0987d
Ashitha Nair <67650148+ashithanair@users.noreply.github.com>
2021-02-17 11:19:52 +0530

    Update Pallindrome.java

--- Pallindrome.java
+++ Pallindrome.java
@@ -18,3 +18,34 @@ class Pallindrome
         System.out.println("The no: is not a pallindrome");
   }
 }
+
+       /*        OR      */
+
+import java.util.Scanner;
+public class Pallindrome 
+{
+	public static void main(String[] args)
+	{
+		int new_num=0,count=0;
+		System.out.println("Enter the no: to be checked");
+		Scanner sc =new Scanner(System.in);
+		int num=sc.nextInt();
+		
+		for(int i=num;i>0;i=(i/10))
+		{
+			int rem=i%10;
+			new_num=new_num*10+rem;
+			
+		}
+		System.out.println(new_num);
+		if(new_num==num)
+		{
+			System.out.println("The given no: is a pallindrome");
+		}
+		else
+		{
+			System.out.println("The given no: is not a pallindrome");
+		}
+																									
+	}
+}
