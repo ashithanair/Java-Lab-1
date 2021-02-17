60fd5951425e94af97d98f7229d50622bd1a5b96
Ashitha Nair <67650148+ashithanair@users.noreply.github.com>
2021-02-17 11:13:49 +0530

    Update Factor.java

--- Factor.java
+++ Factor.java
@@ -1,16 +1,52 @@
-import java.io.*;
-class Factor
+import java.util.Scanner;
+public class Prime 
 {
-  public static void main(String[] args)
-  {
-    int num=15,factor=0;
-    for(int i=1;i<=num;i++)
-    {
-      if(num%i==0)
-      {
-        factor=i;
-        System.out.println(factor+" is a factor of "+num);
-      }
-    }  
-  }
-}
+	public static void main(String[] args)
+	{
+					int flag=0;
+					System.out.println("Enter the no: ");
+					Scanner sc=new Scanner(System.in);
+					int num=sc.nextInt();
+					int m=num/2;
+					if(num==0 || num==1)
+					{
+						System.out.println("The no: is neither prime nor consonant");
+					}
+					else
+					{
+						for(int i=2;i<=m;i++)
+						{
+							if(num%i!=0)
+							{
+								flag=1;
+								
+							}
+							else
+							{
+								flag=0;
+								break;
+							}
+						}
+					
+					
+					
+						if(flag==1)
+						{
+							System.out.println("The given no: is prime");
+						}
+						else 
+						{
+							System.out.println("The given no: is not prime");
+						}
+							
+					}	
+
+			
+		}
+		
+		
+	}
+	
+	
+
+
